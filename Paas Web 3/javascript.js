d5b52842334fb8b0e293fb49351fd001dcf07397
krydge <k.rydge.stubbs@gmail.com>
2019-09-24 19:26:18 -0600

    Add files via upload
    added some javascript functions for fun

---
+++ Paas Web 3/javascript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("submit").disabled = true;
+    document.getElementById("demo").innerHTML = "You clicked the disable button. This made it so you cant submit";
+   }
+
---
+++ Paas Web 3/myFuntion2.js
@@ -0,0 +1,4 @@
+function undoFunction() {
+    document.getElementById("submit").disabled = false;
+    document.getElementById("demo").innerHTML = "You clicked the Undo button. This made it so you can submit";
+}
