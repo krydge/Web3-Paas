5223108f5d9fa3897f95c4d1e90a63480b4321b1
krydge <k.rydge.stubbs@gmail.com>
2019-09-27 10:38:20 -0600

    Update insert.php
    Changed the input stream to a different format

--- Paas Web 3/insert.php
+++ Paas Web 3/insert.php
@@ -1,4 +1,4 @@
-<?php
+/*<?php
 
 // PHP Data Objects(PDO) Sample Code:
 try {
@@ -24,6 +24,37 @@ $sql = "insert_into Name (name, lastname) VALUES
 ('$first_name', '$last_name')";
 
 if(sqlsrv_query($conn, $sql){
+    echo "Added Succesfullly.";
+} else{
+    echo "Error: Not able to execute $sql. " . mysqli_error($link);
+}
+
+//close the connection
+sqlsrv_close($conn);
+?>*/
+
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:mkpaas.database.windows.net,1433; Database = NameSQL", "mkpaas", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "mkpaas@mkpaas", "pwd" => "{your_password_here}", "Database" => "NameSQL", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mkpaas.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+//insert query   
+//set variables
+$query = "INSERT INTO Name VALUES (?, ?)";
+$parameters = [$_POST["firstname"], $POST["lastname"]];
+
+if(sqlsrv_query($conn, $query, $parameters){
     echo "Added Succesfullly.";
 } else{
     echo "Error: Not able to execute $sql. " . mysqli_error($link);
